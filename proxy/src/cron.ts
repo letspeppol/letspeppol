f270dc20418548d4d566603ec8f5c5ed634083f7
Michiel de Jong <michiel@pondersource.com>
2025-11-04 15:38:01 +0100

    make a start with cron fetcher

--- proxy/src/cron.ts
+++ proxy/src/cron.ts
@@ -1,16 +1,18 @@
-import { run } from 'syncables';
-
-// ...
-run();
-
-// ...
-// runOAuthClient({
-//   authorizationURL: 'https://accounts.google.com/o/oauth2/v2/auth',
-//   tokenURL: 'https://oauth2.googleapis.com/token',
-//   clientID: process.env.GOOGLE_CLIENT_ID || ((): void => { throw new Error('GOOGLE_CLIENT_ID not set') })(),
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET || ((): void => { throw new Error('GOOGLE_CLIENT_SECRET not set') })(),
-//   callbackURL: 'http://localhost:8000/callback'
-// }, 8000, async (token) => {
-//   console.log(`Received OAuth token: ${token}`);
-// }); // Start the OAuth client on port 8000
-// console.log('Data fetched and inserted. Visit http://localhost:8000/ if you need to renew the GOOGLE_OAUTH_TOKEN env var.');
+export async function checkForIncomingDocs(): Promise<void> {
+  console.log('Checking for incoming documents...');
+  const res = await fetch(
+      `https://apitest.scrada.be/v1/company/${process.env.SCRADA_COMPANY_ID}/peppol/inbound/document/unconfirmed`,
+    {
+        headers: {
+          'X-Api-Key': process.env.SCRADA_API_KEY!,
+          'X-Password': process.env.SCRADA_API_PWD!,
+        },
+      },
+    );
+  if (!res.ok) {
+    console.error('Failed to fetch incoming documents:', res.statusText);
+    return;
+  }
+  const documents = await res.json();
+  console.log('Incoming documents:', documents);
+}
--- proxy/src/main.ts
+++ proxy/src/main.ts
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import { startServer, ServerOptions } from './server.js';
+import { checkForIncomingDocs } from './cron.js';
 
 // ...
 startServer(process.env as unknown as ServerOptions);
+setInterval(checkForIncomingDocs, 5 * 60 * 1000); // Check every 5 minutes
+checkForIncomingDocs(); // Initial check on startup
