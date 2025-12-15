d680e1c0c03cef3d31b3a180af08d8af1f2adb70
BartBM <bart_swennen@hotmail.com>
2025-12-15 21:07:28 +0100

    #105 Menu buttons should show full list

--- app/ui/src/product/products.ts
+++ app/ui/src/product/products.ts
@@ -4,4 +4,8 @@ import {ProductContext} from "./product-context";
 export class Products {
     private productContext = resolve(ProductContext);
 
+    detaching() {
+        this.productContext.clearSelectedProduct();
+    }
+
 }
