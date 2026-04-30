521a2115937a63d83a9c92b4fc644cd609b6091f
Matthias Ds <admin@satersoft.be>
2026-04-30 20:27:42 +0200

    fix: i18n dynamic for credit note or invoice (#243)
    Co-authored-by: Matthias Desard <matthias@satersoft.be>

--- app/ui/src/invoice/edit/components/modals/invoice-number-modal.ts
+++ app/ui/src/invoice/edit/components/modals/invoice-number-modal.ts
@@ -1,10 +1,15 @@
 import {bindable} from "aurelia";
+import {DocumentType} from "../../../../services/app/invoice-service";
 
 export class InvoiceNumberModal {
     @bindable invoiceContext;
     open = false;
     invoiceNumber: string = '';
     sendFunction = () => { }
+
+    get isCreditNote(): boolean {
+        return this.invoiceContext?.selectedDocumentType === DocumentType.CREDIT_NOTE;
+    }
 
     showModal(sendFunction: () => void) {
         this.sendFunction = sendFunction;
