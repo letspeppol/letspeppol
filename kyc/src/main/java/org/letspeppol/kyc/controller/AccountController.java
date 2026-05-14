34b742076526ea0898669df227499b65f87c0845
0x42-be <git@0x42.be>
2026-05-14 20:22:24 +0200

    OpenAPI documentation added for AccountController

--- kyc/src/main/java/org/letspeppol/kyc/controller/AccountController.java
+++ kyc/src/main/java/org/letspeppol/kyc/controller/AccountController.java
@@ -1,5 +1,8 @@
 package org.letspeppol.kyc.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.letspeppol.kyc.dto.OwnershipSummary;
 import org.letspeppol.kyc.service.JwtService;
@@ -17,12 +20,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/sapi/account")
 @RequiredArgsConstructor
+@Tag(name = "KYC Account", description = "Authenticated account endpoints for discovering which companies and roles are linked to the current identity.")
+@SecurityRequirement(name = "bearerAuth")
 public class AccountController {
 
     private final JwtService jwtService;
     private final OwnershipService ownershipService;
 
     @GetMapping("/ownerships")
+    @Operation(summary = "List linked ownerships", description = "Returns the companies, roles, and ownership relationships available to the currently authenticated identity.")
     public ResponseEntity<List<OwnershipSummary>> getOwnerships(@RequestHeader(HttpHeaders.AUTHORIZATION) String authHeader) {
         JwtInfo jwtInfo = jwtService.validateAndGetInfo(authHeader);
         return ResponseEntity.ok(ownershipService.getOwnershipSummaries(jwtInfo.uid()));
