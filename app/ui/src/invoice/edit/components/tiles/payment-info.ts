4af7e70f2b3725e006da2e5b64917580fd1b39f6
0x42-be <git@0x42.be>
2026-06-02 21:52:24 +0200

    Improvements from review

--- app/ui/src/invoice/edit/components/tiles/payment-info.ts
+++ app/ui/src/invoice/edit/components/tiles/payment-info.ts
@@ -5,12 +5,14 @@ import {InvoiceComposer} from "../../../invoice-composer";
 import {InvoiceService, DocumentDirection, DocumentType} from "../../../../services/app/invoice-service";
 import {AlertType} from "../../../../components/alert/alert";
 import moment from "moment";
+import {I18N} from "@aurelia/i18n";
 
 export class PaymentInfo {
     private readonly ea: IEventAggregator = resolve(IEventAggregator);
     private invoiceService = resolve(InvoiceService);
     private invoiceContext = resolve(InvoiceContext);
     private invoiceComposer = resolve(InvoiceComposer);
+    private readonly i18n = resolve(I18N);
 
     @bindable readOnly: boolean;
     @bindable showPaymentModal;
@@ -62,13 +64,13 @@ export class PaymentInfo {
             await this.invoiceService.togglePaidDocument(document.id);
             if (document.paidOn) {
                 document.paidOn = undefined;
-                this.ea.publish('alert', {alertType: AlertType.Success, text: "Invoice marked as unpaid"});
+                this.ea.publish('alert', {alertType: AlertType.Success, text: this.i18n.tr('alert.invoice.marked-unpaid')});
             } else {
                 document.paidOn = datePaid;
-                this.ea.publish('alert', {alertType: AlertType.Success, text: "Invoice marked as paid"});
+                this.ea.publish('alert', {alertType: AlertType.Success, text: this.i18n.tr('alert.invoice.marked-paid')});
             }
         } catch {
-            this.ea.publish('alert', {alertType: AlertType.Danger, text: "Failed to change invoice paid status"});
+            this.ea.publish('alert', {alertType: AlertType.Danger, text: this.i18n.tr('alert.invoice.paid-status-failed')});
         }
     }
 
