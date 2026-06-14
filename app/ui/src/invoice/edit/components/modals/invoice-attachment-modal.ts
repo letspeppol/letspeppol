149c95ce9e6a88ecc173252d5821686f2c5bd59d
0x42-be <git@0x42.be>
2026-06-14 17:17:37 +0200

    Fix TS2322

--- app/ui/src/invoice/edit/components/modals/invoice-attachment-modal.ts
+++ app/ui/src/invoice/edit/components/modals/invoice-attachment-modal.ts
@@ -140,10 +140,10 @@ export class InvoiceAttachmentModal {
         return true;
     }
 
-    toBase64 = file => new Promise((resolve, reject) => {
+    toBase64 = (file: File): Promise<string> => new Promise((resolve, reject) => {
         const reader = new FileReader();
         reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
+        reader.onload = () => resolve(reader.result as string);
         reader.onerror = reject;
     });
 
