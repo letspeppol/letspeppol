5c6b29c4461c0b43b9203ec375c35d2a0b5ce345
bits-craft <bits-craft@hotmail.com>
2026-08-02 20:56:22 +0200

    removed test path in KboXmlSync..

--- kyc/src/main/java/org/letspeppol/kyc/service/kbo/KboXmlSyncService.java
+++ kyc/src/main/java/org/letspeppol/kyc/service/kbo/KboXmlSyncService.java
@@ -80,10 +80,6 @@ public class KboXmlSyncService {
         kboSftpClient.downloadFile(remoteZipPath, localZip);
 
         Path localXml = extractSingleXml(localZip);
-
-//        Path localXml = Path.of("/opt/documents/bits/letspeppol_docs/D20260307.KBO/D20260307.xml"); // For testing purposes only
-//        String remoteZipName = "D20260307.xml";
-
         log.info("Importing initial KBO XML from {}", localXml);
         try (InputStream in = Files.newInputStream(localXml)) {
             kboXmlParserService.importEnterprises(in);
